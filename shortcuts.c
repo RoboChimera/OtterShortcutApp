bdc780703bb62a70724ab4a118f8d5ed4460eaf7
HelloRoboRobo <gitlab@HelloRoboRobo.com>
2023-12-08 22:12:07 +0100

    Added one extra button, I want to see if I can make a config file to change how many shortcuts etc etc

--- shortcuts.c
+++ shortcuts.c
@@ -6,11 +6,27 @@
 static GtkWidget *grid;
 static GtkWidget *shortcut1;
 
-void do_shortcut() {
-	system("xterm -e top");
+void do_shortcut(int i) {
+	// Handles the actual shortcut cases
+	switch (i) {
+		case 1:
+			system("xterm -e top &");
+			break;
+		default:
+			system("echo 'Invalid Shortcut' &");
+			break;
+	}
+}
+
+void button_click(GtkWidget *widget, gpointer data) {
+	int *shortcut = (int *)data;
+	do_shortcut(*shortcut);
 }
 
 int main(int argc, char *argv[]) {
+    int shortcutID1 = 1;
+    int shortcutID2 = 2;
+
     // Initialize GTK
     GtkWidget *window, *grid, *shortcut1;
     gtk_init(&argc, &argv);
@@ -28,10 +44,13 @@ int main(int argc, char *argv[]) {
     grid = gtk_grid_new();
     gtk_container_add(GTK_CONTAINER(window), grid);
 
-    shortcut1 = gtk_button_new_with_label("top");
-    g_signal_connect(shortcut1, "clicked", G_CALLBACK(do_shortcut), NULL);
+    shortcut1 = gtk_button_new_with_label("Execute top");
+    g_signal_connect(shortcut1, "clicked", G_CALLBACK(button_click), &shortcutID1);
     gtk_grid_attach(GTK_GRID(grid), shortcut1, 0, 0, 1, 1);
 
+    shortcut1 = gtk_button_new_with_label("shortcut1");
+    g_signal_connect(shortcut1, "clicked", G_CALLBACK(button_click), &shortcutID2);
+    gtk_grid_attach(GTK_GRID(grid), shortcut1, 1, 0, 1, 1);
     // Start the GTK main loop
     gtk_widget_show_all(window);
     gtk_main();
